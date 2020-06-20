6f94183572c42d151f3f95239fcf1a4a49cc8d16
suesalito <suesalito@gmail.com>
2020-06-20 23:03:40 +0700

    commit the drop down picker and platform selection.

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -18,6 +18,7 @@ const List<String> currenciesList = [
   'RUB',
   'SEK',
   'SGD',
+  'THB',
   'USD',
   'ZAR'
 ];
--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,11 +1,97 @@
 import 'package:flutter/material.dart';
+import 'coin_data.dart';
+import 'package:flutter/cupertino.dart';
+import 'dart:io' show Platform;
+//const List<String> currencyList = ['USD', 'EUR', 'THB'];
 
 class PriceScreen extends StatefulWidget {
   @override
   _PriceScreenState createState() => _PriceScreenState();
 }
 
+List<DropdownMenuItem<String>> dropDrowList = [];
+
 class _PriceScreenState extends State<PriceScreen> {
+  DropdownButton<String> androidDropDownButton() {
+    List<DropdownMenuItem<String>> outputList = [];
+
+    //for (int i = 0; i < currencyList.length; i++) {
+
+    //   outputList.add(
+    //     DropdownMenuItem(
+    //       child: Text(currencyList[i]),
+    //       value: currencyList[i],
+    //     ),
+    //   );
+
+    // You can also use " For in" in this case which will return the samething.
+    for (String currency in currenciesList) {
+      outputList.add(
+        DropdownMenuItem(
+          child: Text(currency),
+          value: currency,
+        ),
+      );
+
+      //print(currencyList[i]);
+    }
+    //return ;
+    return DropdownButton<String>(
+      value: selectedCurrency,
+      dropdownColor: Colors.black,
+      items: outputList,
+      onChanged: (value) {
+        setState(
+          () {
+            selectedCurrency = value;
+            print(value);
+          },
+        );
+      },
+    );
+  }
+
+  CupertinoPicker iOSPicker() {
+    List<Widget> pickerList = [];
+    for (String currency in currenciesList) {
+      pickerList.add(Text(currency));
+
+      //print(currencyList[i]);
+    }
+    //return pickerList;
+    return CupertinoPicker(
+      itemExtent: 32,
+      onSelectedItemChanged: (selectedIndex) {
+        print(selectedIndex);
+      },
+      scrollController: FixedExtentScrollController(initialItem: 20),
+      children: pickerList,
+    );
+  }
+
+  Widget getPicker() {
+    if (Platform.isIOS) {
+      print('this is ios');
+      return iOSPicker();
+    } else if (Platform.isAndroid) {
+      print('this is android');
+      return androidDropDownButton();
+    }
+  }
+
+  String selectedCurrency = 'USD';
+
+  @override
+  void initState() {
+    // TODO: implement initState
+  }
+
+  @override
+  void deactivate() {
+    // TODO: implement deactivate
+    super.deactivate();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -41,9 +127,13 @@ class _PriceScreenState extends State<PriceScreen> {
             height: 150.0,
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
-            color: Colors.lightBlue,
-            child: null,
-          ),
+            color: Colors.pink[200],
+            width: 20,
+            //color: Colors.lightBlue,
+            //child: androidDropDownButton(),
+            child: getPicker(),
+          )
+          //child:
         ],
       ),
     );
